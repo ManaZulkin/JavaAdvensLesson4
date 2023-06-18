237c03d5ceabd22728bfcc81fb65826244e2f9d6
Taras <thezulkin@gmail.com>
2023-06-18 13:39:48 +0200

    time+

--- src/task3/Time.java
+++ src/task3/Time.java
@@ -1,9 +1,7 @@
 package task3;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import javax.sound.midi.Soundbank;
+import java.util.*;
 
 public class Time {
     public static void main(String[] args) {
@@ -13,16 +11,26 @@ public class Time {
         int month = input();
         System.out.println("Enter day");
         int day = input();
+        System.out.println("Enter hours");
+        int hours = input();
+        System.out.println("Enter minutes");
+        int min = input();
+        System.out.println("Enter sek");
+        int sek = input();
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
         gregorianCalendar.set(Calendar.YEAR, year);
         gregorianCalendar.set(Calendar.MONTH, month - 1);
         gregorianCalendar.set(Calendar.DAY_OF_MONTH, day);
+        gregorianCalendar.set(Calendar.HOUR, hours);
+        gregorianCalendar.set(Calendar.MINUTE, min);
+        gregorianCalendar.set(Calendar.SECOND, sek);
         System.out.println("Date:\n" + gregorianCalendar.getTime());
         tillNow(gregorianCalendar);
     }
 
     public static void tillNow(GregorianCalendar gc){
         GregorianCalendar gc1 = new GregorianCalendar();
+        System.out.println("From that date past:");
         System.out.println("Year - " + (gc1.get(Calendar.YEAR) - gc.get(Calendar.YEAR)));
         if ((gc1.get(Calendar.MONTH) - gc.get(Calendar.MONTH)) > 0){
             System.out.println("Month - " + (gc1.get(Calendar.MONTH) - gc.get(Calendar.MONTH)));
@@ -30,6 +38,16 @@ public class Time {
         if ((gc1.get(Calendar.DAY_OF_MONTH) - gc.get(Calendar.DAY_OF_MONTH)) > 0){
             System.out.println("Days - " + (gc1.get(Calendar.DAY_OF_MONTH) +  gc.get(Calendar.DAY_OF_MONTH)));
         } else System.out.println("Days - " + Math.abs(gc1.get(Calendar.DAY_OF_MONTH) + 30  - gc.get(Calendar.DAY_OF_MONTH)) );
+        if ((gc1.get(Calendar.HOUR) - gc.get(Calendar.HOUR)) > 0){
+            System.out.println("Hours - " + (gc1.get(Calendar.HOUR) - gc.get(Calendar.HOUR)));
+        } else System.out.println("Hours - " + Math.abs(gc1.get(Calendar.HOUR) + 24 - gc.get(Calendar.HOUR)) );
+        if ((gc1.get(Calendar.MINUTE) - gc.get(Calendar.MINUTE)) > 0){
+            System.out.println("Minutes - " + (gc1.get(Calendar.MINUTE) - gc.get(Calendar.MINUTE)));
+        } else System.out.println("Minutes - " + Math.abs(gc1.get(Calendar.MINUTE) + 60 - gc.get(Calendar.MINUTE)) );
+        if ((gc1.get(Calendar.SECOND) - gc.get(Calendar.SECOND)) > 0){
+            System.out.println("Sekonds - " + (gc1.get(Calendar.SECOND) - gc.get(Calendar.SECOND)));
+        } else System.out.println("Sekonds - " + Math.abs(gc1.get(Calendar.SECOND) + 60 - gc.get(Calendar.SECOND)) );
+
     }
 
     public static int input(){
